4692fdbfffd78d46e3ed30e8ff46bab49339e2e4
GhostsRd <111060306+GhostsRd@users.noreply.github.com>
2026-03-24 14:23:18 +0300

    Update ExampleTest.php

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,14 +15,5 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
-    public function test_it_redirects_correctly(): void
-{
-    $response = $this->get('/');
 
-    // On change 200 par 302 car ton middleware redirige
-    $response->assertStatus(302);
-    
-    // Optionnel : vérifier la destination
-    // $response->assertRedirect('/login');
-}
 }
